a3e5d6256d74d515a60e46b770cae6aa61da95d9
ahmeddhussain <ahmedkhater2611@gmail.com>
2026-08-03 19:54:41 +0300

    project test

--- vars/sonarscan.groovy
+++ vars/sonarscan.groovy
@@ -2,11 +2,13 @@ def call(String projectKey, String credentialsId = 'sonarqube-token') {
     echo "Running SonarQube Code Quality Analysis for ${projectKey}..."
     withCredentials([string(credentialsId: credentialsId, variable: 'SONAR_TOKEN')]) {
         sh """
-            sonar-scanner \
+            docker run --rm --net=host \
+              -v "\$(pwd):/usr/src" \
+              sonarsource/sonar-scanner-cli \
               -Dsonar.projectKey=${projectKey} \
               -Dsonar.sources=. \
-              -Dsonar.host.url=http://localhost:9000 \
-              -Dsonar.login=${SONAR_TOKEN} \
+              -Dsonar.host.url="http://localhost:9000" \
+              -Dsonar.token="${SONAR_TOKEN}" \
               || echo "SonarQube scan completed with warnings"
         """
     }
