72e31d82aa336fd3be1577e3171b71432c91b0bb
ahmeddhussain <ahmedkhater2611@gmail.com>
2026-08-03 20:08:40 +0300

    project test

--- vars/pushimage.groovy
+++ vars/pushimage.groovy
@@ -1,8 +1,13 @@
 def call(String awsRegion, String ecrUrl, String imageName, String imageTag) {
     echo "Logging into AWS ECR and pushing ${imageName}:${imageTag}..."
-    sh """
-        aws ecr get-login-password --region ${awsRegion} | docker login --username AWS --password-stdin ${ecrUrl}
-        docker tag ${imageName}:${imageTag} ${ecrUrl}/${imageName}:${imageTag}
-        docker push ${ecrUrl}/${imageName}:${imageTag}
-    """
+    withCredentials([
+        string(credentialsId: 'AWS_ACCESS_KEY_ID', variable: 'AWS_ACCESS_KEY_ID'),
+        string(credentialsId: 'AWS_SECRET_ACCESS_KEY', variable: 'AWS_SECRET_ACCESS_KEY')
+    ]) {
+        sh """
+            aws ecr get-login-password --region ${awsRegion} | docker login --username AWS --password-stdin ${ecrUrl}
+            docker tag ${imageName}:${imageTag} ${ecrUrl}/${imageName}:${imageTag}
+            docker push ${ecrUrl}/${imageName}:${imageTag}
+        """
+    }
 }
