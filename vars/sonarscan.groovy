003b217783e8830fe3e302b184cd318ed95acb5e
ahmeddhussain <ahmedkhater2611@gmail.com>
2026-08-04 13:41:31 +0300

    monitoring

--- vars/sonarscan.groovy
+++ vars/sonarscan.groovy
@@ -1,21 +1,14 @@
 def call(String projectKey, String sourceDir = ".", String credentialsId = 'sonarqube-token') {
     echo "Running SonarQube Code Quality Analysis for ${projectKey}..."
     withCredentials([string(credentialsId: credentialsId, variable: 'SONAR_TOKEN')]) {
-sh """
-    docker run --rm --net=host \
-      --memory="512m" \
-      --memory-swap="512m" \
-      -e SONAR_SCANNER_OPTS="-Xmx400m" \
-      -v "\$(pwd):/usr/src" \
-      sonarsource/sonar-scanner-cli \
-      -Dsonar.projectKey=${projectKey} \
-      -Dsonar.sources=${sourceDir} \
-      -Dsonar.host.url="http://localhost:9000" \
-      -Dsonar.token="\${SONAR_TOKEN}" \
-      -Dsonar.java.binaries=. \
-      -Dsonar.exclusions="**/node_modules/**,**/target/**,**/*.jar,**/*.war,**/*.png,**/*.jpg,**/*.jpeg,**/*.svg" \
-      -Dsonar.javascript.node.maxspace=512 \
-      || echo "SonarQube scan completed with warnings"
-"""
+        sh """
+            sonar-scanner \
+              -Dsonar.projectKey=${projectKey} \
+              -Dsonar.sources=${sourceDir} \
+              -Dsonar.host.url="http://localhost:9000" \
+              -Dsonar.token="\${SONAR_TOKEN}" \
+              -Dsonar.java.binaries=. \
+              -Dsonar.exclusions="**/node_modules/**,**/target/**,**/*.jar,**/*.war,**/*.png,**/*.jpg,**/*.jpeg,**/*.svg"
+        """
     }
 }
